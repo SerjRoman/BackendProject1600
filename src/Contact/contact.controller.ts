589acc51dc534348cc9f735fd97eaa68349f3865
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-06-22 12:30:50 +0300

    i tried

--- src/Contact/contact.controller.ts
+++ src/Contact/contact.controller.ts
@@ -1,32 +1,52 @@
 import { NextFunction, Request, Response } from "express";
 import { ContactService } from "./contact.service";
+import { failure } from "../tools/result";
+import { ErrorCodes } from "../types/error-codes";
 
 export const ContactController = {
-	getContactById: async function (req: Request, res: Response, next: NextFunction) {
-        const id = req.params.id;
-        // if NaN
-		const result = await ContactService.getContactById(+id)
+	getContactById: async function (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		const id = req.params.id;
+		// if NaN
+		const result = await ContactService.getContactById(+id);
 		if (result.status == "failure") {
 			next(result);
 			return;
 		}
 		res.json(result);
 	},
-	getAllContacts: async function (req: Request, res: Response, next: NextFunction) {
-		const result = await ContactService.getAllContacts()
+	getAllContacts: async function (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		const userId: number = res.locals.userId;
+		if (!userId) {
+			next(failure(ErrorCodes.UNAUTHORIZED));
+			return;
+			// throw failure(ErrorCodes.UNAUTHORIZED)
+		}
+		const result = await ContactService.getAllContacts(userId);
 		if (result.status == "failure") {
 			next(result);
-            return;
+			return;
 		}
 		res.json(result);
 	},
-	createContact: async function (req: Request, res: Response, next: NextFunction) {
-        const body = req.body;
-		body.ownerId = res.locals.userId
-		const result = await ContactService.createContact(body)
+	createContact: async function (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) {
+		const body = req.body;
+		body.ownerId = res.locals.userId;
+		const result = await ContactService.createContact(body);
 		if (result.status == "failure") {
 			next(result);
-            return;
+			return;
 		}
 		res.json(result);
 	},
--- src/Contact/contact.repository.ts
+++ src/Contact/contact.repository.ts
@@ -2,17 +2,25 @@ import { client, PrismaKnownError } from "../prisma/client";
 import { Prisma } from "../generated/prisma";
 import { Result, failure, success } from "../tools/result";
 import { ErrorCodes, PrismaErrorCodes } from "../types/error-codes";
-import { ContactWhereUnique, Contact, CreateContact, ReceivedContact } from "./contact.types";
+import {
+	ContactWhereUnique,
+	Contact,
+	CreateContact,
+	ReceivedContact,
+} from "./contact.types";
+import { User } from "../User/user.types";
 
 export const ContactRepository = {
-    getContact: async function (where: ContactWhereUnique): Promise<Result<Contact>> {
-        try {
-            const contacts = await client.contact.findUniqueOrThrow({
-                where: where
-            })
-            return success(contacts)
-        } catch (err) {
-            if (err instanceof PrismaKnownError) {
+	getContact: async function (
+		where: ContactWhereUnique
+	): Promise<Result<Contact>> {
+		try {
+			const contacts = await client.contact.findUniqueOrThrow({
+				where: where,
+			});
+			return success(contacts);
+		} catch (err) {
+			if (err instanceof PrismaKnownError) {
 				switch (err.code) {
 					case PrismaErrorCodes.NOT_EXIST:
 						return failure(ErrorCodes.NOT_FOUND);
@@ -22,30 +30,46 @@ export const ContactRepository = {
 				}
 			}
 			return failure(ErrorCodes.UNHANDLED);
-        }
-    },
-    getAllContacts: async function (): Promise<Result<Contact[]>> {
-        try {
-            const contacts = await client.contact.findMany()
-            return success(contacts)
-        } catch (err) {
-            if (err instanceof PrismaKnownError) {
+		}
+	},
+	//for one user
+	getAllContacts: async function (userId: number): Promise<Result<User[]>> {
+		try {
+			const contacts = await client.user.findMany({
+				where: {
+					OR: [
+						{
+							contacts: {
+								some: { ownerId: userId },
+							},
+						},
+					],
+					NOT: {
+						id: userId,
+					},
+				},
+			});
+			return success(contacts);
+		} catch (err) {
+			if (err instanceof PrismaKnownError) {
 				switch (err.code) {
 					default:
 						return failure(ErrorCodes.UNHANDLED);
 				}
 			}
 			return failure(ErrorCodes.UNHANDLED);
-        }
-    },
-    createContact: async function (data: ReceivedContact): Promise<Result<Contact>> {
-        try {
-            const contact = await client.contact.create({
-                data: data
-            })
-            return success(contact)
-        } catch (err) {
-            if (err instanceof PrismaKnownError) {
+		}
+	},
+	createContact: async function (
+		data: ReceivedContact
+	): Promise<Result<Contact>> {
+		try {
+			const contact = await client.contact.create({
+				data: data,
+			});
+			return success(contact);
+		} catch (err) {
+			if (err instanceof PrismaKnownError) {
 				switch (err.code) {
 					case PrismaErrorCodes.UNIQUE:
 						return failure(ErrorCodes.EXISTS);
@@ -55,6 +79,6 @@ export const ContactRepository = {
 				}
 			}
 			return failure(ErrorCodes.UNHANDLED);
-        }
-    }
-}
+		}
+	},
+};
